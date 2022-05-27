d5f629adb0ba0f9acab7b05223a74db242a2ce20
Shane Jackson <shane.jackson@gmx.co.uk>
2022-05-27 10:51:53 +0100

    updated installation requirmenets

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 import setuptools
+from os import path
 
+#set location for reading requirmenets txt
+HERE = path.abspath(path.dirname(__file__))
+
+# Parse requirements.txt file so to have one single source of truth
+REQUIREMENTS_DATA = []
+with open(path.join(HERE, "requirements.txt"), encoding="utf-8") as f:
+    for l in f.readlines():
+        if not l.startswith("#"):
+            if ">=" in l:
+                REQUIREMENTS_DATA.append([l.split(">=")[0]])
+            elif "=" in l:
+                REQUIREMENTS_DATA.append([l.split("=")[0]])
+
+#configuratiuon
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
@@ -8,6 +23,7 @@ setuptools.setup(
     author="Shane Jackson",
     author_email="shane.jackson@gmx.co.uk",
     description="Scraping panel data from the Grants on the Web webpages",
+    install_requires = REQUIREMENTS_DATA,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shanej90/gotw_webscraping",
